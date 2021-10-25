a100f295fa152d72f48e2789d9626b2985920bfb
dogfootdev <no10305@gmail.com>
2021-10-25 23:47:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-6f8myd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-6f8myd)
---
+++ index.ts
@@ -0,0 +1,8 @@
+let startTime = new Date().getTime();
+async function test() {
+  const { SchemaLink } = await import('apollo-link-schema');
+  console.log('async time 3 : ' + (new Date().getTime() - startTime));
+}
+console.log('start time : ' + (new Date().getTime() - startTime));
+test();
+console.log('end time : ' + (new Date().getTime() - startTime));
